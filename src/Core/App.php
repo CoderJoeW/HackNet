26cff507eb48f4635c459f659bb999ae99d987fb
CoderJoeW <j.williamson@gambler-labs.com>
2023-05-09 18:49:17 -0400

    consistent formatting and update default route

--- src/Core/App.php
+++ src/Core/App.php
@@ -4,25 +4,25 @@ namespace Hacknet\Core;
 
 use FastRoute;
 
-class App
-{
+class App{
     private $autoloader;
     private $request;
     private $router;
 
-    public function __construct()
-    {
+    public function __construct(){
         $this->autoloader = new Autoloader();
         $this->request = new Request();
         $this->router = new Router();
     }
 
-    public function run()
-    {
+    public function run(){
         spl_autoload_register([$this->autoloader, 'autoloadControllers']);
 
         $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
-            $r->addRoute('GET', '/{controller}/{action}', 'dynamic_route');
+            // Default route
+            $r->addRoute('GET', '/', ['controller' => 'Home', 'action' => 'index']);
+            // Dynamic route
+            $r->addRoute('GET', '/{controller}[/{action}]', 'dynamic_route');
         });
 
         $uri = $this->request->getUri();
